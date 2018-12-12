a81ccdf9181d92f8fe26738f04a4d6b7525f02ef
ricarsant <rsantacruzg@dian.gov.co>
2018-12-12 11:44:45 -0500

    Version ajusta movilidad campos.
    Se adiciona Funcionalidad de "RUTA"

--- senado.js
+++ senado.js
@@ -1,43 +1,33 @@
 
-document.body.style.zoom = 0.80
+//document.body.style.zoom = 0.80
  svg = d3.select("svg"),
     width = +svg.attr("width"),
     height = +svg.attr("height"),
     margin = {top: 30, right: 20, bottom: 80, left: 70};
 	
 var arregloCampos=[];
-//var datos;
+
+var gLink;
+var gNode;
+var root;
+var ancho_line=4;
+
 function dAngular(dato){
 	var arreglo_campos=[];
-	console.log("de Angular a D3, en D3 dato:", dato, dato.length);
 
 	
 	for(i =0;i<dato.length;i++){
 		campo = dato[i].text;
 		arreglo_campos.push(campo);
-		console.log("adentro", "--", campo);
 	}
 	arregloCampos =arreglo_campos;
-	console.log("de Angular a D3, en D3  arreglo_campos", arreglo_campos);
-	
-	console.log("datosxxxxxx", datosnew);
+	d3.select("#slider").selectAll("svg").remove();
 	d3.select("#selecion").selectAll("svg").remove();
+	
 	senado ("senado.csv");
-	//showTree(datosnew);
-	//brushed();
 	
 }
 
-
-
- 
-   ////**** PATH
-
-
-
-//////*FIN FUNCION SEARCH*////
-
- 
 
 function senado (senado){
 
@@ -48,21 +38,14 @@ var margin1 = 20,
 
 var x1 = d3.scaleTime()
 			.range([0,width1]);
-			
-//var datos;
-//var encabezados =[];
 
 d3.csv(senado,function(error, dataok){
 		if(error) throw error
 datos = dataok;
 encabezados =Object.keys(datos[0]);
-		console.log("Envabezados en d3",encabezados);
-		console.log("ENCABEZADOS1 de ANGULAR A D3 quemados, en D3",encabezados1);
-		//console.log("Ordenados de angular a d3",filedsToD3);
 recive(encabezados);		
 		
 var titulos ={};
-//valores_nodos={"Comision Tercera de Senado":50};
 valores_nodos ={};
 
 var parseDate = d3.timeParse("%Y-%m-%d");
@@ -70,18 +53,14 @@ var parseDate = d3.timeParse("%Y-%m-%d");
 datos = datos.sort((a,b) => d3.ascending(a.fecha_radicacion,b.fecha_radicacion));
 
 datos.forEach(function(d){
-	
-//var propertyName =  d.id_proyecto;
-//var propertyValue = d.titulo;
-//titulos[propertyName] =propertyValue;
 d.fecha_radicacion = parseDate(d.fecha_radicacion);
+d.estado_actual_fecha = parseDate(d.estado_actual_fecha);
 });
  
  x1
   .domain(d3.extent(datos,function(d){return d.fecha_radicacion}))
 			
 var tempo = 	d3.extent(datos,function(d){return d.fecha_radicacion});
-console.log("extent", tempo);
  	
 var x = d3.scaleLinear()
         .domain([0,100])
@@ -101,19 +80,37 @@ var x = d3.scaleLinear()
         .call(d3.axisBottom()
             .scale(x1)
             .ticks(8));
-
    var brushg = svg1.append("g")
         .attr("class", "brush")
 		.style("stroke","blue")
-        .call(brush)
+		.call(brush);
+   brush.move(brushg,  [x1(parseDate("2014-01-01")),x1(parseDate("2018-01-01"))]);
    
-		// brush.move(brushg, x1.range());
- 
-   brush.move(brushg,  [x1(parseDate("2015-01-01")),x1(parseDate("2016-01-01"))]);
+   var slider1 = d3.sliderHorizontal()
+    .min(4)
+    .max(10)
+    .width(300)
+    .ticks(10)
+    .default(0)
+    .on('onchange', val => {
+      ancho_line = val;
+	  
+	  var buscax = document.getElementById('filterInput').value;
+
+		  buscaProyecto();
+	  
+    });
+	
+	
+   var group1 = d3.select("div#slider").append("svg")
+    .attr("width", 400)
+    .attr("height", 90)
+	.attr("transform", "translate(50,30)");
+
+ group1.call(slider1);
  
  
  function brushed() {
-      console.log ("gggd",datos);
       var range = d3.brushSelection(this)
           .map(x1.invert);
 		  yeard = range[0].getFullYear();
@@ -129,34 +126,30 @@ var x = d3.scaleLinear()
 		
 		d3.selectAll("#fechamarca")
 		.text(rangoFecha);
-		
-		
-		/*
-      d3.selectAll("span")
-          .text(rangoFecha)
-		  //.style("color","#D4DFED");
-		  */
-		  
 		  d3.select("#svg").selectAll(".arbol").remove();
- //var startDate = "2015-08-04";
  datosnew = datos.filter(
       function(d){
 return  (d.fecha_radicacion) >= range[0] && (d.fecha_radicacion) <= range[1]; 
 	 
  });
  
- console.log("new new",datosnew);
+ 
  
  		  showTree(datosnew);
     }
-} //fin cvs(function(){
-); //fin cvs(
+} 
+); 
 
-}//new fin senado
+}
 
-var gLink;
-var gNode;
-var root;
+function fecha_texto(fecha){
+		 var yearh = fecha.getFullYear();
+		 var mesh = fecha.getMonth()+1;
+		 var diah = fecha.getDate();
+		var rangoFecha =  yearh + "/" + mesh + "/" + diah;
+		return rangoFecha 
+	
+}
 
 function showTree(datos){
 
@@ -164,25 +157,15 @@ if(arregloCampos.length ==0){
 	arregloCampos = encabezados;
 	
 }
-	console.log("de Angular a D3, en D3  arreglo_campos en showtree", arregloCampos);
-/*
-data = d3.nest()
-			//.key(function(d){return d[encabezados[0]];})
-			.key(function(d){return d.comision_entrante;})
-			.key(function(d){return d.iniciativa;})
-			.key(function(d){return d.estado_actual;})
-			.key(function(d){return d.id_proyecto;})
-			.entries(datos);
-			
-data01 = d3.nest()
-			.key(function(d){return d.comision_entrante;})
-			.rollup(function(v) { return v.length; })
-			.entries(datos);
-*/
+	
 titulos ={};
 datos.forEach(function(d){
 var propertyName =  d.id_proyecto;
-var propertyValue = d.titulo;
+var valor = d.titulo  +"<br><br><b> Fecha Radicacion: &nbsp;&nbsp;&nbsp;&nbsp;</b> " + fecha_texto(d.fecha_radicacion)  + "<br><b> Fecha Estado Actual:</b> " + fecha_texto(d.estado_actual_fecha) ;
+var timeDiff = Math.abs(d.estado_actual_fecha.getTime() - d.fecha_radicacion.getTime());
+var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+valor = valor + "<br><b> Numero dias:</b> " + diffDays;
+var propertyValue = valor;
 titulos[propertyName] =propertyValue;
 });
 
@@ -210,26 +193,11 @@ data01.forEach(function(d){
 	total = total+d.value;
 });
 
-//Rescala = d3.scale.sqrt().domain([1, total]).range([10, 39]);
-console.log("totall ",total)
-console.log("comisiones", valores_nodos);
-
-
-console.log("data nest",data);
-			
 treeObj = new Object();
 
 treeObj.name = "Crongreso";
 children =[];
 data = creaTree(data);
-
-//console.log("datata tree",data);
-
-
-///**********
-
-
- //const root = d3.hierarchy(data);
  
 root = d3.hierarchy(data);
 
@@ -238,61 +206,24 @@ root = d3.hierarchy(data);
   root.x0 = dy / 2;
   root.y0 = 0;
   
-  
-  
-  /*
   root.descendants().forEach((d, i) => {
-	  
-	 console.log("d Interno",d);
-	
-   
-  });
-  */
-  
-  root.descendants().forEach((d, i) => {
-	  
-	// console.log("d Interno",d);
     d.id = i;
     d._children = d.children;
     if (d.depth && d.data.name.length !== 7) d.children = null;
   });
-
-  console.log("DATA HIERRACHI ROOT",root);
-  
-  
-  
   val_nodes={};
-	//poner_valores_nodos(root);
-  
-  
-  
-  
- // root.count();
- // console.log("The leaf count is "+root.value);
-//console.log("The leaves are: "+root.leaves().map( d => d.data.name ));
-  
   svg
 	  .attr("width", width)
       .attr("height", dx)
       .attr("viewBox", [-margin.left, -margin.top, width, dx])
       .style("font", "10px sans-serif")
       .style("user-select", "none");
-  
-   //console.log(svg);
-   
-  
-   
-   
-   
-   //const gLink = svg.append("g")
    gLink = svg.append("g")
 	.attr("class","arbol")
       .attr("fill", "none")
       .attr("stroke", "#555")
       .attr("stroke-opacity", 0.4)
       .attr("stroke-width", 1.5);
-
-  //const gNode = svg.append("g")
   gNode = svg.append("g")
   .attr("class","arbol")
       .attr("cursor", "pointer");
@@ -300,120 +231,42 @@ root = d3.hierarchy(data);
     
     tree = d3.tree().nodeSize([dx, dy]);
 	 diagonal = d3.linkHorizontal().x(d => d.y).y(d => d.x);
-	 
-	 //se pasa a update
-	
-	 ////**************************************AQUI VA UPDATE *****************
-	 
-	 
-	 ///////////////////////////////////FIN  UPDATE************************
-	 
-	 
-   
-   
    update(root);
    
-
-	
- 
- 
-
-   //val_nodes
    function poner_valores_nodos(root){
-	   console.log("Funcion valores",root);
-	   
-	   
-	   ///
-	   
 	   root.descendants().forEach((d, i) => {
-	  
-	  
-	 /* 
-	  d.sum(function(d) {
-		if(d._children.length != "undefined"){
-		return d._children.length;} })
-	  ;
-	  */
 	  total =0;
 	  if(d._children != "undefined"){
 		  
 		  
 	  }
-	  console.log("d Interno d",d);
-  
-	// console.log("d Interno suma",d._children._children._children.length);
-  
-    
-  }); //fin foreach
+  }); 
 
-   
- // root.count();
- // console.log("The leaf count is "+root.value);
-//console.log("The leaves are: "+root.leaves().map( d => d.data.name ));
-  
 	   
-	   ///
-	   
-   } //fin poner_valores_nodos
+   } 
    
    
 
-}  //showtree
- 
-    
-//} ; // aqui original fin funcyion senado
-
-
-//////NUEVA POSICION OTRAS FUNCIONES
-
+}  
 function buscaProyecto(){
 	
 	var buscax = document.getElementById('filterInput').value;
-	//console.log("Valor buscax",buscax,titulos);
 	
 
 	if(isNaN(buscax)){
-		//var encuentra = false;
 		buscax = buscax.toLowerCase();
-		console.log("Valor buscax1",buscax, titulos);
-		
 		for(const item in titulos){
 			titulo = titulos[item].toLowerCase();
 			
 			if(titulo.includes(buscax)){
 				buscax = item;
-				//enceuntra = true;
-				break;
-			}
-			//console.log("titulos" , titulos[item]);
-		}
-		
-		
-	/*
-		for(i=0;i<titulos.length;i++){
-			id = titulos[i].id_proyecto;
-			titulo = titulos[i].titulo;
-			console.log("Valor titulos ****",id, titulo);
-			titulo = titulo.toLowerCase();
-			if(titulo.includes(buscax)){
-				buscax = id;
 				break;
 			}
 		}
-		*/
 	}
-	/*
-	alert(buscax);
-	 var ubica = searchTree(root,buscax,[]);
-	 openPaths(ubica);
-	
-*/	
-console.log("****************salida**************",buscax);
 
 	if(!isNaN(buscax)){
 		 var ubica = searchTree(root,buscax,[]);
-		 //alert(buscax);
-  console.log("*********************busqueda... ", ubica);
   
   if(typeof ubica !== "undefined" && ubica.length>0){
  openPaths(ubica);
@@ -431,14 +284,8 @@ console.log("****************salida**************",buscax);
 	 
 } 
 
- ////**** PATH
-
 function openPaths(paths){
 	colorpath =  randDarkColor(); 
-	//color1 =  ColorLuminance(colorpath,-0.6);
-	//console.log("color colorpath ", colorpath, color1);
-
-
 	
 		for(var i =0;i<paths.length;i++){
 			if(paths[i].id !== "1"){//i.e. not root
@@ -452,17 +299,7 @@ function openPaths(paths){
 			}
 		}
 	}
-
-
-////**** FIN PATH
-
-
-
-
-//////*FUNCION SEARCH*////
-
 	function searchTree(obj,search,path){
-		console.log("serach **************************",obj);
 		if(obj.data.name === search){ //if search is found return, add the object to the path and return it
 			path.push(obj);
 			return path;
@@ -485,49 +322,10 @@ function openPaths(paths){
 		}
 	}
 
-
-
-//////*FIN FUNCION SEARCH*////
-
-
-
-
-//////FIN OTRAS FUNCIONES
-
-
-
-////////////////NUEVA APOSICION UPDATE
-
 function update(source) {
     const duration = d3.event && d3.event.altKey ? 2500 : 250;
     const nodes = root.descendants().reverse();
     const links = root.links();
-	
-	console.log("NODOS control",nodes);
-	
-	/*
-	 //viene de Arriba (tree)
-	 
-	   const gLink = svg.append("g")
-	.attr("class","arbol")
-      .attr("fill", "none")
-      .attr("stroke", "#555")
-      .attr("stroke-opacity", 0.4)
-      .attr("stroke-width", 1.5);
-
-  const gNode = svg.append("g")
-  .attr("class","arbol")
-      .attr("cursor", "pointer");
-
-    
-    tree = d3.tree().nodeSize([dx, dy]);
-	 diagonal = d3.linkHorizontal().x(d => d.y).y(d => d.x);
-
-	 ///fin viene
-	
-	*/
-	
-    // Compute the new tree layout.
     tree(root);
 
     let left = root;
@@ -544,14 +342,6 @@ function update(source) {
         .attr("height", height)
         .attr("viewBox", [-margin.left, left.x - margin.top, width, height])
         .tween("resize", window.ResizeObserver ? null : () => () => svg.dispatch("toggle"));
-		
-		/*
-		var div = d3.select("body").append("div")
-    .attr("class", "tooltip")
-    .style("opacity", 0);
-	*/
-		
-		
 		var myTool = d3.select("body")
                   .append("div")
                   .attr("class", "tooltip1")
@@ -569,7 +359,6 @@ function update(source) {
         .attr("stroke-opacity", 0)
         .on("click", d => {
           d.children = d.children ? null : d._children;
-		  console.log("*************HACE CLICK************",d);
           update(d);
         });
 
@@ -591,9 +380,6 @@ function update(source) {
 				}
 				
 			}else 
-				
-			console.log("Esta en NODEENTER*******************")
-			
 			return d._children ? '9' : '5';
 		
 			
@@ -606,8 +392,6 @@ function update(source) {
         .attr("x", d => d._children ? -15 : 15)
         .attr("text-anchor", d => d._children ? "end" : "start")
 		  .text(d => {
-			  
-			  console.log("%%%%%%% append text !!!!!");
 		   if(d.data.name.indexOf("/") >= 1){
 			  
 			   return "Mixta";
@@ -621,25 +405,6 @@ function update(source) {
 	      
 	
 	   )
-	   
-	
-		   
-	  
-	   /*
-	   .on("mouseover",function(d){
-		 d3.select(this)    
-		  .append("title")
-		  .html(function(d){
-			 if (typeof(d.data.children)== 'undefined'){
-				var id_titulo =d.data.name
-			  return "Titulo :" + titulos[id_titulo];
-			 }
-			 
-		  });
-	 })
-	 */
-	
-	   
 	    .on("mouseover", function(d){  //Mouse event
 		if (typeof(d.data.children)== 'undefined'){
 				var id_titulo =d.data.name;
@@ -647,8 +412,6 @@ function update(source) {
                 d3.select(this)
                     .transition()
                     .duration(500)
-					
-                    //.attr("x", function(d) { return x(d.cocoa) - 30; })
                     .style("cursor", "pointer")
                     .attr("width", 60)
                     myTool
@@ -669,7 +432,6 @@ function update(source) {
                 d3.select(this)
                     .transition()
                     .duration(500)
-                    //.attr("x", function(d) { return x(d.cocoa) - 20; })
                     .style("cursor", "normal")
                     .attr("width", 40)
                     myTool
@@ -678,73 +440,26 @@ function update(source) {
                         .style("opacity", "0")            
                         .style("display", "none")  //The tooltip disappears
               });
-		
-   //console.log("**********veamos  node****",nodeEnter);
-	  
-	   
 	    nodeEnter.append('text')
-					/*
-			        .attr('x', function(d){
-						if( d.children && d.children.length>=10) return -8;
-						else if(d.children) return -3;
-						
-						if( d._children && d._children.length>=10) return -8;
-						else if(d._children) return -3;
-					})
-					*/
-					
 			        .attr('y', 3)
 			        .attr('cursor', 'pointer')
 			        .style('font-size', '10px')
 			        .text(function(d) {
-						 //total =0;
-						console.log("dddd appnde TEXT*******************",d);
 						
 			        	if (d.children && d.depth==0) return total; //d.children.length;
 			        	else if (d._children) //return   //valores_nodos[d.data.name]    //d._children.length;
 						{
-							
-							//return d._children.length;
-							console.log("valores D",d);
 							if(d.depth ==1){
 								return valores_nodos[d.data.name] ;
 							}
 							if(d.depth==2){
 								subtotal =0;
-								/*
-								if (typeof(subtotal) == 'undefined') {
-								subtotal =0;}	
-									else{subtotal = subtotal+1}
-									*/
 									nodos_desc = d.descendants();
-								console.log("decendientes",nodos_desc)
 							
 							for(i=0;i<nodos_desc[0]._children.length;i++){
 								subtotal = subtotal + nodos_desc[0]._children[i]._children.length;
 							};
-								
-								/*
-								nodos_desc.forEach(function(n){
-									console.log(n.data.name, "longitud children cada descendiente",n._children.length);
-									
-									for(i=0;i<n._children.length;i++){
-										//if( n._children[i].hasOwnProperty("._children") && n._children[i]._children !== "undefined" && n._children[i]._children.hasOwnProperty("length") )
-									if( n._children[i]._children != "undif" )
-										{
-										subtotal = subtotal + n._children[i]._children.length;
-										console.log("cada uno ",i, " -- ", n.data.name , " ---",  n._children[i], " -- ", n._children[i]._children)
-										
-										}else{
-											//subtotal = subtotal+n._children.length
-											subtotal = subtotal+1;
-											}
-									}
-									
-								})
-								*/
-								//console.log("SUBTOTAL: ",subtotal);
 								d.value = subtotal;
-								console.log("d value ", d.value);
 								return subtotal;
 							}
 							
@@ -754,27 +469,6 @@ function update(source) {
 							}
 							
 							return d._children.length;
-							
-							//desc = d.descendants();
-							//console.log("des",desc);
-							//console.log("The descendants are: "+d.descendants().map( d1 => d1.data.name ));
-						
-						/*
-						d.sum(function(d1){ return d1.children.length; });
-							total =	 total+d.value 
-							console.log("d name " ,d.value );
-							console.log("deep ",d.height)
-							return total;
-							
-							*/
-							//console.log("childre. ",d3.sum(d.data.children.length))
-							
-							//console.log("adentro",d.name.length);
-							/*
-							for(i=0;i<d._children.length;i++){
-								console.log("adentro" ,d._children[i].length);
-							}
-							*/
 							
 						}
 			        })
@@ -811,26 +505,16 @@ function update(source) {
         .attr("stroke-linejoin", "round")
         .attr("stroke-width", 3)
         .attr("stroke", "white");
-
-		
-		
-		
     // Transition nodes to their new position.
     const nodeUpdate = node.merge(nodeEnter).transition(transition)
         .attr("transform", d => `translate(${d.y},${d.x})`)
         .attr("fill-opacity", 1)
         .attr("stroke-opacity", 1);
-		
-	
-
     // Transition exiting nodes to the parent's new position.
     const nodeExit = node.exit().transition(transition).remove()
         .attr("transform", d => `translate(${source.y},${source.x})`)
         .attr("fill-opacity", 0)
         .attr("stroke-opacity", 0);
-		
-		
-			
 
     // Update the links…
     const link = gLink.selectAll("path")
@@ -849,33 +533,15 @@ function update(source) {
         .attr("d", diagonal)
 		
 		.style("stroke",function(d){
-			console.log("elemme stroke", d)
 				if(d.target.class!="" ){
 					return d.target.class;
 				}
 			})
-			
-		//stroke-width="2" fill-opacity="0.5"	
 		.style("stroke-width", function(d){
 			if(typeof d.target.class!="undefined" ){
-					return 4;
+					return ancho_line;
 				}else {return 1;}
 		})
-	/*
-		.style("stroke",function(d){
-			console.log("elemme stroke", d)
-				if(d.target.class==="found" ){
-					if(typeof colorFind !== 'undefined'){
-					//return "#ff4136";
-					return colorFind
-					}else  
-						return "#ff4136";
-					d.target.color = colorFind;
-					d.target.class = "xxx";
-				}
-			});
-		*/
-		
     // Transition exiting nodes to the parent's new position.
     link.exit().transition(transition).remove()
         .attr("d", d => {
@@ -891,16 +557,7 @@ function update(source) {
 	
 	
 	
-  }  //fin update
-
-
-////////////////FIN NUEVA UPDATE
-
-
-///////funcion colorDepth
-
-
-
+  } 
 
 function randDarkColor() {
     var lum = -0.25;
@@ -920,15 +577,11 @@ function randDarkColor() {
 
 
 function ColorLuminance(hex, lum) {
-
-	// validate hex string
 	hex = String(hex).replace(/[^0-9a-f]/gi, '');
 	if (hex.length < 6) {
 		hex = hex[0]+hex[0]+hex[1]+hex[1]+hex[2]+hex[2];
 	}
 	lum = lum || 0;
-
-	// convert to decimal and change luminosity
 	var rgb = "#", c, i;
 	for (i = 0; i < 3; i++) {
 		c = parseInt(hex.substr(i*2,2), 16);
@@ -939,15 +592,12 @@ function ColorLuminance(hex, lum) {
 	return rgb;
 }
 
-/////fin funcion colorDepth
-
 
 function creaTree(data){
 	
 	data.forEach(function(d){
 	objItem = {};
 	objItem.name = d.key;
-	//subchildren =[];
 	if(d.key.indexOf("/")>=1 ){
 		nomb = d.key.split("/");
 		subchildren =[];
